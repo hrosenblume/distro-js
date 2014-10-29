96780bc19d32e7469703de092291c7af28eddfa3
Louis Williams <louieaw@gmail.com>
2014-10-28 23:10:17 -0400

    Added test html

--- distro-js-serv/services/PrimeDSS.js
+++ distro-js-serv/services/PrimeDSS.js
@@ -1,9 +1,19 @@
 /**
  * Created by AltonjiC on 10/4/14.
  */
-//var mongoose = require('../mongoose');
-//var PrimeJob = mongoose.model('PrimeJob');
-//var serv = require("service.js");
+
+
+this.onmessage = function(event) {
+    if (event.data.task == 'kill') {
+        console.log('KILL');
+    } else if (event.data.task == 'init') {
+        console.log('INIT');
+        init();
+    } else if (event.data.task == 'jobFinished') {
+        console.log("JOB FINISHED");
+        onJobFinished(event.data.result);
+    }
+}
 
 function init() {
     //start = new Date().getTime();
@@ -18,8 +28,9 @@ function init() {
         var counter;
         var total_cycles = 0;
         var results = new Array();
+        console.log("Starting with: ", start_x);
         //body bulk
-        for (var x = start_x; x < x+5; x++) {
+        for (var x = start_x; x < start_x + 1000; x++) {
             counter = 2;
             is_prime = true;
             if (x % 2 == 0) {is_prime = false;}
@@ -28,54 +39,57 @@ function init() {
                     is_prime = false;
                 }
                 counter++;
+                console.log(counter);
             }
             total_cycles+=(counter-1);
-            results.push([x,is_prime]);
+            results.push({value: x, prime: is_prime});
+            console.log(x, is_prime);
+
         }
         //end, metric calculations, and return
         var internal_end = new Date().getTime();
         var internal_time = internal_end - internal_start;
         internal_time = internal_time / 1000; //end time in
-        var avg_cycles = total_cycles / (max - 3);
-        console.log(avg_cycles); //avergae clock cycles
-        console.log(time); // time
-        results.push([avg_cycles, internal_time]); //add metrics into it
+        // var avg_cycles = total_cycles / (max - 3);
+        // console.log(avg_cycles); //avergae clock cycles
+        console.log(internal_time); // time
+        results.push({time: internal_time}); //add metrics into it
         callback(results);
     }
 
-    var raw_prime_search = function() {
-        //variables
-        var internal_start = new Date().getTime();
-        var is_prime = true;
-        var total_cycles = 0;
-        var results = new Array();
-        //body bulk
-        for (var x = 3; x <= max; x++) {
-            for (var y = 2; y < x ; y++) {
-                if (x % y == 0) {
-                    is_prime = false;
-                }
-            }
-            results.push([x,is_prime]);
-            total_cycles += x;
-            is_prime = true;
-        }
-        //closing
-        var internal_end = new Date().getTime();
-        var internal_time = internal_end - internal_start;
-        var avg_cycles = total_cycles / (max - 3);
-        console.log(avg_cycles); //avergae clock cycles
-        console.log(time); // time
-        results.push([avg_cycles, internal_time]); //add metrics into it
-    }
+    // var raw_prime_search = function() {
+    //     //variables
+    //     var internal_start = new Date().getTime();
+    //     var is_prime = true;
+    //     var total_cycles = 0;
+    //     var results = new Array();
+    //     //body bulk
+    //     for (var x = 3; x <= max; x++) {
+    //         for (var y = 2; y < x ; y++) {
+    //             if (x % y == 0) {
+    //                 is_prime = false;
+    //             }
+    //         }
+    //         results.push([x,is_prime]);
+    //         total_cycles += x;
+    //         is_prime = true;
+    //     }
+    //     //closing
+    //     var internal_end = new Date().getTime();
+    //     var internal_time = internal_end - internal_start;
+    //     var avg_cycles = total_cycles / (max - 3);
+    //     console.log(avg_cycles); //avergae clock cycles
+    //     console.log(time); // time
+    //     results.push([avg_cycles, internal_time]); //add metrics into it
+    // }
 
     var functionAsString = "" + preferred_prime_search;
     //var functionAsString = "" + raw_prime_search;
 
     //add the jobs
-    for (var x = 3; x < MAX; x+=DIFF) {
+    // for (var x = 3; x ; x+=DIFF) {
         postMessage({task: 'sendJob', fn: functionAsString, params: x});
-    }
+    // }
 }
 
 function onJobFinished(contents) {
